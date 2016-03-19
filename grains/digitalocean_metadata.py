0a5858b61002f6d21bc4be0ee835c1350c9aecbd
Iggy <iggy@theiggy.com>
2016-03-19 16:35:01 -0700

    Handle the case where the instance is too old to support metadata
    On instances that existed before the metadata service was added to
    Digital Ocean, this grain threw errors. It also throws errors on
    non-digitalocean systems. Cleaned up both of those minor glitches by
    adding a __virtual__ function that catches both corner cases and doesn't
    load the module in that case.

--- grains/digitalocean_metadata.py
+++ grains/digitalocean_metadata.py
@@ -9,23 +9,51 @@
     ~~~~~~~~~~~~~~~~~~~~~~~
 
     Create a DigitalOcean grain from the DigitalOcean metadata server.
-    See https://developers.digitalocean.com/metadata/#metadata-in-json
+    See https://developers.digitalocean.com/documentation/metadata/#metadata-in-json
 
     Note that not all datacenters were supported when this feature was first
     released.
 '''
 
 # Import Python Libs
+import logging
 import requests
+
+# Set up logging
+LOG = logging.getLogger(__name__)
+
+MD_BASE_URI = "http://169.254.169.254/metadata/v1"
+__virtualname__ = "digitalocean"
+
+
+def __virtual__():
+    '''
+    We should only load if this is actually a digitalocean instance
+
+    Also catch if the instance predates the metadata service
+    '''
+    try:
+        ret = requests.get(MD_BASE_URI + '.json')
+        if ret.text == "":
+            return False
+        return __virtualname__
+    except requests.ConnectionError:
+        LOG.info('digitalocean grain: Instance too old to support metadata')
+        return False
+    except Exception:
+        return False
 
 
 def digitalocean():
     '''
     Return DigitalOcean metadata.
     '''
-    do_svr = 'http://169.254.169.254/metadata/v1.json'
-    metadata = requests.get(do_svr)
+    metadata = requests.get(MD_BASE_URI + '.json')
 
     if metadata.status_code == 200:
         return {'digitalocean': metadata.json()}
     return {'digitalocean': []}
+
+
+if __name__ == '__main__':
+    print digitalocean()
